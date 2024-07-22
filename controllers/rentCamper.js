e37b2805a3cbf9a94b23a512a04787a0ac845ae5
Kristina <kr.hranovska@gmail.com>
2024-07-22 19:53:44 +0300

    Update rentCamper.js

--- controllers/rentCamper.js
+++ controllers/rentCamper.js
@@ -2,13 +2,9 @@ import { Camper } from "../models/camper.js";
 
 const rentCamper = async (req, res, next) => {
     try {
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 4;
-
+        const { page = 1, limit = 4 } = req.query;
         const skip = (page - 1) * limit;
-
-        const result = await Camper.find().skip(skip).limit(limit);
-
+        const result = await Camper.find().skip(skip).limit(parseInt(limit));
         res.json(result);
     } catch (error) {
         next(error);
