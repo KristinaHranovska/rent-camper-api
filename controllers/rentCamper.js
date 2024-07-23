eaab654cb2053813c2c8ba46e40079843fc26d5a
Kristina <kr.hranovska@gmail.com>
2024-07-23 13:32:10 +0300

    Update rentCamper.js

--- controllers/rentCamper.js
+++ controllers/rentCamper.js
@@ -2,13 +2,30 @@ import { Camper } from "../models/camper.js";
 
 const rentCamper = async (req, res, next) => {
     try {
-        const { page = 1, limit = 4 } = req.query;
+        const { page = 1, limit = 4, location, form, details } = req.query;
         const skip = (page - 1) * limit;
 
-        const total = await Camper.countDocuments();
+
+        const filter = {};
+        if (location) {
+            filter.location = new RegExp(location, 'i');
+        }
+        if (form) {
+            filter.form = form;
+        }
+        if (details) {
+            filter.details = {};
+            Object.keys(details).forEach(key => {
+                if (details[key]) {
+                    filter.details[key] = true;
+                }
+            });
+        }
+
+        const total = await Camper.countDocuments(filter);
         const totalPages = Math.ceil(total / limit);
 
-        const result = await Camper.find().skip(skip).limit(parseInt(limit));
+        const result = await Camper.find(filter).skip(skip).limit(parseInt(limit));
         res.json({ data: result, totalPages });
     } catch (error) {
         next(error);
