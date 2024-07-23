9b4603b26e5ac28a3754c923b3ba24be16fb6983
Kristina <kr.hranovska@gmail.com>
2024-07-23 10:30:07 +0300

    Update books.js

--- models/books.js
+++ models/books.js
@@ -1,37 +1,6 @@
 import { model, Schema } from 'mongoose';
 import Joi from 'joi';
 
-/**
- * @swagger
- * components:
- *   schemas:
- *     Booking:
- *       type: object
- *       required:
- *         - name
- *         - email
- *         - date
- *       properties:
- *         name:
- *           type: string
- *           description: The name of the person booking the camper.
- *         email:
- *           type: string
- *           format: email
- *           description: The email of the person booking the camper.
- *         date:
- *           type: string
- *           format: date
- *           description: The date of the booking.
- *         comment:
- *           type: string
- *           description: Additional comments for the booking.
- *       example:
- *         name: John Doe
- *         email: john.doe@example.com
- *         date: 2023-08-15
- *         comment: Looking forward to the trip!
- */
 const bookingSchema = new Schema({
     name: {
         type: String,
@@ -48,7 +17,7 @@ const bookingSchema = new Schema({
     },
     comment: {
         type: String,
-        required: false
+        default: "",
     }
 });
 
